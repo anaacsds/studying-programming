ece1b48326a3ff05bb026922c15518d16831dec7
Ana Clara <anacsds1@gmail.com>
2025-11-11 21:09:44 -0300

    feat: exercicio vetores 1

---
+++ C#/Vetores/Inverso de um vetor/inverso.cs
@@ -0,0 +1,10 @@
+/* Faça um programas em C# para:
+a) gerar um vetor V2, que é o inverso de outro vetor V1:
+V1[2,31,4,55] => V2[55,4,31,2]
+b) gerar um vetor V3 que é a concentração de outros dois vetores V1 e V2:
+V1[1,9,14,20] V2[3,21,9] V3[1,9,14,20,3,21,9]
+c) gerar um vetor V3 que é a intercolação ordenada de outros dois vetores ordenados V1 e V2:
+V1[9,15,33] V2[11,21,29,35]
+V3[...]
+*/
+
