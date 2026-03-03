55c44696af800886e2dede5192faaa4eb98676ff
Ana Clara <anacsds1@gmail.com>
2026-03-03 17:09:40 -0300

    atividades preencher vetor

---
+++ C#/Vetores/Preencher 1-10/preench.cs
@@ -0,0 +1,10 @@
+/*Faça um vetor que preencha os números de 1 a 10*/
+
+int [] V = new int [10];
+int i, cont = 1;
+
+for (i=0; i < V.Length; i ++)
+{
+    Console.Write(V[i] = cont);
+    cont++;
+}
