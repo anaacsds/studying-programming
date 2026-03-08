a2536aa59071a33bed4588ca4a7e06042dce6f9c
Ana Clara <anacsds1@gmail.com>
2026-03-07 21:32:42 -0300

    feat: 1.0 livro

---
+++ Python/exercicios-livro-entendendo-algoritmos/ex1.0/ex.py
@@ -0,0 +1,14 @@
+# 1.1 Suponha que você tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria o
+# número máximo de etapas que levaria para encontrar o número desejado?
+
+# Pesquisa simpples: 128 etapas necessárias
+
+# Pesquisa binária: log128 = 2 elevado a 7
+
+
+
+# 1.2 Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?
+
+# Pesquisa simpples: 256 etapas necessárias
+
+# Pesquisa binária: log256 = 2 elevado a 8
