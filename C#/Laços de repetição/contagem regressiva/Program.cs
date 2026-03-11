0cc0af88ccef205bd3f12c74adde8c147acc89c8
Ana Clara <anacsds1@gmail.com>
2026-03-11 16:59:18 -0300

    feat: contagem regressiva

---
+++ C#/Laços de repetição/contagem regressiva/Program.cs
@@ -0,0 +1,40 @@
+﻿// Escreva um algoritmo que receba um número e mostre a contagem regressiva dele até zero, 
+// apenas com os números pares.
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+
+        Console.WriteLine("Informe um número inteiro positivo: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] cont = contagem(n);
+
+        Console.Write($"Contagem regressiva de {n} até 0 (apenas números pares): ");
+        for(int i = 0; i < cont.Length; i++)
+        {
+            Console.Write($"{cont[i]} , ");
+            
+        }
+    }
+
+    static int[] contagem(int n)
+    {
+        int tamanho = (n / 2) + 1;
+        int[] r = new int[tamanho];
+        int pos = 0;
+
+        for (int i = n; i >= 0; i--)
+        {
+            if (i % 2 == 0)
+            {
+                r[pos] = i;
+                pos++;          
+            }
+        }
+        return r;
+    }
+}
