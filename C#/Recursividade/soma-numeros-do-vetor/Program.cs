e34c6abf7e277ed1ebf8290f4b91ac4a8fec21cf
Ana Clara <anacsds1@gmail.com>
2026-03-24 19:51:23 -0300

    soma dos números do vetor - 20 posições

---
+++ C#/Recursividade/soma-numeros-do-vetor/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Faça uma função recursiva capaz de imprimir em tela a soma dos elementos de um vetor de 20 posições
+composto de números aleatórios.
+*/
+
+class Program {
+    static void Main(string[]args) {
+
+        int result;
+        int i = 0;
+        int [] v = new int[20];
+
+        PreenchV(v, i);
+        result = SomaVAleat(v, i);
+
+        Console.WriteLine(result);
+    }
+
+    static int[] PreenchV(int[]v, int i) {
+        Random x = new Random();
+        //Preenchendo vetor
+        if(i < v.Length) {
+            v[i] = x.Next(1,50);
+
+            return PreenchV(v, i + 1);
+        }
+        return v;
+    }
+
+    static int SomaVAleat(int[]v, int i) {
+
+        int soma = 0;
+
+        if(i < v.Length) 
+        soma = v[i] + SomaVAleat(v, i + 1);
+        
+        return soma;
+        }
+    }
