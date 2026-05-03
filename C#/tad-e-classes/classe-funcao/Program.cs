562171408e1b1a373d77af4dbeaab3b23bbbf0a1
Ana Clara <anacsds1@gmail.com>
2026-05-03 19:19:14 -0300

    classe com função

---
+++ C#/tad-e-classes/classe-funcao/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Crie:
+Classe Produto
+Nome
+Preço
+
+Depois faça uma função:
+
+MostrarProduto(Produto p)
+
+Ela deve mostrar:
+
+Nome
+Preço */
+
+class Produto
+{
+    private string Nome;
+    private double preco;
+    public string nome
+    {
+        get{return Nome;}
+        set{Nome = value;}
+    }
+    public double Preco
+    {
+        get{return preco;}
+        set {preco = value;}
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Produto p = new Produto();
+    p.nome = "Coca Cola 200ml";
+    p.Preco = 2.50;
+    MostrarProduto(p);   
+    }
+
+    static void MostrarProduto(Produto p)
+    {
+        Console.WriteLine($"Nome: {p.nome}");
+        Console.WriteLine($"Preço: {p.Preco}");
+    }
+
+}
