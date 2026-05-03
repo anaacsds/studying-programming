1bdc6bd15710bd5117868b86369c07a692aca78f
Ana Clara <anacsds1@gmail.com>
2026-05-03 18:34:46 -0300

    vetor com 5 nomes

---
+++ C#/Vetores/vetor-5-nomes/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Faça:
+Um vetor de 5 nomes
+Leia os nomes
+Mostre todos*/
+
+string []v = new string [5];
+
+for(int i = 0; i < v.Length; i++)
+{
+    Console.WriteLine($"Nome {i+1}: ");
+    v[i] = Console.ReadLine();
+}
+
+Console.Clear();
+
+for(int i = 0; i < v.Length; i ++)
+{
+    Console.Write($"{v[i]} - ");
+}
