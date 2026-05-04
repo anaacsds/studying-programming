fe717a38c6163a629759ed63bf71338542fcb4c2
Ana Clara <anacsds1@gmail.com>
2026-05-04 17:40:32 -0300

    recursividade soma aleatorios

---
+++ C#/Recursividade/soma-numeros-do-vetor-2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Faça uma função recursiva capaz de imprimir em tela a soma dos elementos de um vetor de 20 posições
+composto de números aleatórios. */
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        int [] v = new int[20];
+        Random x = new Random();
+        int soma = 0, i = 0;
+
+        for (int j = 0; j < v.Length; j++)
+        v[j] = x.Next(1, 100);
+
+        somaV(v, soma, i);
+
+        Console.WriteLine($"Soma: {somaV(v, soma, i)}");
+    }
+
+    static int somaV(int [] v, int s, int i)
+    {
+       s += v[i];
+
+       if (i < v.Length - 1)
+        return somaV(v, s, i + 1);
+
+        else
+        return s;
+    }
+}
