10ae48b7e4f76d5f01ca733dcd1254ec249f9b1f
Ana Clara <anacsds1@gmail.com>
2026-05-05 19:15:51 -0300

    recursividade potencia

---
+++ C#/Recursividade/potencia/Program.cs
@@ -0,0 +1,24 @@
+﻿// Crie uma função que calcule recursivamente a potência 𝑥𝑦 , sendo 𝑥 e 𝑦 lidos pelo teclado.
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Digite um número para ser elevado: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Digite a que número {x} será elevado: ");
+        int y = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"O resultado é: {potencia(x, y)}");
+    }
+    static int potencia (int x, int y)
+    {
+        if(y == 0)
+        return 1;
+
+        else
+            return x * potencia(x, y - 1);
+
+    }
+}
