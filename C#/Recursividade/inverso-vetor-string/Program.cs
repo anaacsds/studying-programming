78a70d8fdf3cfcf7423528a69a6e0154d51c4e77
Ana Clara <anacsds1@gmail.com>
2026-05-05 18:43:21 -0300

    recursividade invertendo vetor

---
+++ C#/Recursividade/inverso-vetor-string/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Faça um programa que contenha uma função capaz de inverter uma string fornecida por um usuário.*/
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine($"Digite uma palavra que queira inverter: ");
+        string a = Console.ReadLine();
+
+        int i = a.Length - 1;
+
+        Console.WriteLine($"Palavra invertida: {invert(a, i)}");
+    }
+    static string invert(string a, int i)
+    {
+        if(i < 0)
+        return "";
+
+        else
+        {
+            return a[i] + invert(a, i - 1 );
+        }
+    }
+}
