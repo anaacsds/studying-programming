1acd637d849a4540df7c29f59f2062b3f0f260fd
Ana Clara <anacsds1@gmail.com>
2026-05-06 16:38:40 -0300

    recursividade palindromo

---
+++ C#/Recursividade/palindromo/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Crie bool palindromo(string s, int esq, int dir) que retorne true se a string for um palíndromo.
+
+Exemplo: "arara" → true, "casa" → false. Leia a string pelo teclado. */
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Digite uma palavra para saber se é um palíndromo: ");
+        string a = Console.ReadLine();
+
+        Console.Clear();
+
+        int esq = 0, dir = a.Length - 1;
+        Console.WriteLine($"{a} : {palindromo(a, esq, dir)}");
+    }
+
+    static bool palindromo(string s, int e, int d)
+    {
+        if (e >= d)                                 
+            return true;                           
+
+        if (s[e] != s[d])
+            return false;                           
+
+        return palindromo(s, e + 1, d - 1); 
+    }
+}
