5a51f759963a329374675f042fa932ce88ec924a
Ana Clara <anacsds1@gmail.com>
2026-06-05 18:51:10 -0300

    feat: exercicio de listas

---
+++ C#/lista-pilha-fila/listas/inserir-string/Program.cs
@@ -0,0 +1,4 @@
+﻿/* Faça um programa que leia do teclado uma string qualquer. Crie uma Lista Simplesmente
+Encadeada e não Ordenada que insira cada letra dessa string no início dessa lista ou no final,
+dependendo da opção do usuário. */
+
